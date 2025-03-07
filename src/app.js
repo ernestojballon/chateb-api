6d6952beb7d72abc741f8926df3900a1df0cda36
Ernesto J Ballon <ernestoballono@gmail.com>
2025-03-07 20:58:17 -0300

    adding client

--- src/app.js
+++ src/app.js
@@ -70,9 +70,9 @@ app.use("/api/chats",ClerkExpressRequireAuth(),chatRouter);
 
 app.use("/api/llm",ClerkExpressRequireAuth(), llmRouter); 
 
-app. use(express.static(path.join(__dirname, "./dist")));
+app. use(express.static(path.join(__dirname, "../client")));
 app.get("*", (req, res ) => {
-  res.sendFile(path.join(__dirname, "./dist/index.html"));
+  res.sendFile(path.join(__dirname, "../client/index.html"));
 })
 
 app.use((err, req, res, next) => {
